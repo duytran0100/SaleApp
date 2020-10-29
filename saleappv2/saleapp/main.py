fbafd0e7fb37cd7c33bc832fea2cb10269eb9b4e
duytran0100 <73629604+duytran0100@users.noreply.github.com>
2020-10-29 12:02:20 +0700

    Saleapp

---
+++ saleappv2/saleapp/main.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from saleapp import app,utils
+
+
+@app.route('/')
+def index():
+    categories = utils.read_data()
+    return render_template('index.html',categories=categories)
+
+
+@app.route('/products')
+def product_list():
+    products=utils.read_data("data/products.json")
+    return render_template('product_list.html',products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True)

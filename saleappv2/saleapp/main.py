34d38e02e84c07da97a175ef7e6103be753042c1
duytran0100 <73629604+duytran0100@users.noreply.github.com>
2020-11-05 20:11:39 +0700

    loc du lieu
    ád

--- saleappv2/saleapp/main.py
+++ saleappv2/saleapp/main.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from saleapp import app,utils
 
 
@@ -10,8 +10,18 @@ def index():
 
 @app.route('/products')
 def product_list():
-    products=utils.read_data("data/products.json")
+    cate_id=request.args.get('category_id')
+    kw=request.args.get('keyword')
+    from_price=request.args.get('from_price')
+    to_price=request.args.get('to_price')
+    products=utils.read_product(cate_id=cate_id,keyword=kw,from_price=from_price,to_price=to_price)
     return render_template('product_list.html',products=products)
+
+
+@app.route('/products/<int:product_id>')
+def product_detail(product_id):
+    product = utils.get_product_by_id(product_id=product_id)
+    return render_template('product_detail.html',product=product)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- saleappv2/saleapp/utils.py
+++ saleappv2/saleapp/utils.py
@@ -3,3 +3,27 @@ import json
 def read_data(path="data/categories.json"):
     with open(path,encoding="utf-8") as f:
         return json.load(f)
+
+
+def read_product(cate_id=None,keyword=None,from_price=None,to_price=None):
+    products = read_data(path="data/products.json")
+
+    if cate_id:
+        cate_id = int(cate_id)
+        products = [p for p in products if cate_id == p['category_id']]
+
+    if keyword:
+        products = [p for p in products if p['name'].lower().find(keyword.lower()) >=0]
+
+    if from_price and to_price:
+        from_price = float(from_price)
+        to_price = float(to_price)
+        products = [p for p in products if from_price <= p['price'] <= to_price]
+
+    return products
+
+def get_product_by_id(product_id):
+    products=read_data('data/products.json')
+    for p in products:
+        if p["id"] == product_id:
+            return p

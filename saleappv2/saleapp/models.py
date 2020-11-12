7ac8df05d6566d000db7e75e5687d3c633338451
duytran0100 <duytran0100@gmail.com>
2020-11-12 09:01:59 +0700

    admin

--- saleappv2/saleapp/models.py
+++ saleappv2/saleapp/models.py
@@ -1,18 +1,28 @@
 from saleapp import db
-from sqlalchemy import Integer,Float,String,Column,ForeignKey
+from sqlalchemy import Integer, Float, String, Column, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
 
-class Category(db.Model):
-    id = Column(Integer,primary_key=True,autoincrement=True)
-    name = Column(String(50),nullable=False)
-    products = relationship('Product',backref='category',lazy=True)
 
-class Product(db.Model):
-    id = Column(Integer,primary_key=True,autoincrement=True)
-    name = Column(String(50),nullable=False)
-    price = Column(Float,default=0)
-    description = Column(String(255),nullable=True)
-    category_id = Column(Integer,ForeignKey(Category.id),nullable=False)
+class SaleBase(db.Model):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(SaleBase):
+    products = relationship('Product', backref='category', lazy=True)
+
+
+class Product(SaleBase):
+    price = Column(Float, default=0)
+    description = Column(String(255), nullable=True)
+    image = Column(String(255), nullable=True)
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
 
 if __name__ == '__main__':
     db.create_all()
